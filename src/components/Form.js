8d50a6360f7fdcebd6736845e3301e6d3eeba533
Rodrigo <rodrigo.o.geraldes@gmail.com>
2023-07-15 12:17:33 -0300

    remoção do register em form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,35 @@
-import React from 'react'
-import bgImg from '../assets/img1.jpg';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import bgImg from "../assets/img1.jpg";
+import { useForm } from "react-hook-form";
 
 export default function Form() {
+  const { handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-    const { register, handleSubmit } = useForm()
-    const onSubmit = data => console.log(data);
+  // console.log(watch('username'));
 
-    // console.log(watch('username'));
-    
   return (
     <section>
-        <div className="register">
-            <div className="col-1">
-                <h2>Zendesk App</h2>
-                <span>Insira suas credenciais zendesk</span>
+      <div className="register">
+        <div className="col-1">
+          <h2>Zendesk App</h2>
+          <span>Insira suas credenciais zendesk</span>
 
-                <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-                    <input type="text" {...register("url")} placeholder='URL do ambiente' />
-                    <input type="text" {...register("username")} placeholder='Nome de usuário' />
-                    <input type="password" {...register("password")} placeholder='Senha' />
-                    <button className='btn'>Entrar</button>
-                </form>
-
-            </div>
-            <div className="col-2">
-                <img src={bgImg} alt="" />
-            </div>
+          <form
+            id="form"
+            className="flex flex-col"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <input type="text" placeholder="URL do ambiente" />
+            <input type="text" placeholder="Nome de usuário" />
+            <input type="password" placeholder="Senha" />
+            <button className="btn">Entrar</button>
+          </form>
         </div>
+        <div className="col-2">
+          <img src={bgImg} alt="" />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
